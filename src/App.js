8cdd29cb84734a231367a1bcdf6822eb36a25dac
pvcapuano <pvcapuano@gmail.com>
2021-09-15 21:02:19 -0300

    inserercao dos dados

--- src/App.js
+++ src/App.js
@@ -5,13 +5,41 @@ const api ={
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+const dateBuilder = (d) => {
+  let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Junho", "Julho", "Agosto",
+  "Setembro", "Outubro", "Novembro", "Dezembro"]
+
+  let days = ["Segunda-feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"]
+
+  let day = days[d.getDay()]
+  let date = d.getDate()
+  let month = months[d.getMonth()]
+  let year = d.getFullYear()
+
+  return `${day} ${date} ${month} ${year}`
+
+}
+
 function App() {
   return (
     <div className="App">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Pesquisar..." />
-          
+        </div>
+
+        <div className="location-box">
+          <div className="location">Rio De Janeiro, BR</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">
+            15 graus
+          </div>
+          <div className="weather">
+            Verão
+          </div>
         </div>
       </main>
     </div>
